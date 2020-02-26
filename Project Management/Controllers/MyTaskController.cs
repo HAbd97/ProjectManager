8917a9a4bbccd617facbf3a1471df7cca6f10ba5
swathisv <61487910+swathisv@users.noreply.github.com>
2020-02-26 11:48:47 +0530

    MyTaskController Update

--- Project Management/Controllers/MyTaskController.cs
+++ Project Management/Controllers/MyTaskController.cs
@@ -1,34 +1,50 @@
-﻿using System;
+﻿using Project_Management.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
-using Project_Management.Models;
 
 namespace Project_Management.Controllers
 {
     public class MyTaskController : ApiController
     {
-        ProjectManagerEntities db = new ProjectManagerEntities();
-     public HttpResponseMessage Get()
+        
+        public HttpResponseMessage Get()
         {
-          
-            var result = (from tsk in db.Task_tbl
-             join emp in db.Employee_tbl
-             on tsk.EmpId equals emp.EmpId join proj in db.Project_tbl on tsk.ProjectId equals proj.ProjectId
-             select new CompleteTask
-             {
-                 TaskName = tsk.TaskName,
-                 ProjectName = proj.Project,
-                 EstimatedHours = tsk.EstimatedHours,
-                 ConsumedHours = tsk.ConsumedHours,
-                 Deviation=tsk.Deviation,
-                 Status=tsk.Status
+            ProjectManagerEntities1 db = new ProjectManagerEntities1();
 
-             }).ToList();
+            var result = (from tsk in db.Task_tbl
+                          join emp in db.Employee_tbl
+                          on tsk.EmpId equals emp.EmpId
+                          join proj in db.Project_tbl on tsk.ProjectId equals proj.ProjectId
+                          select new CompleteTask
+                          {
+                              TaskName = tsk.TaskName,
+                              ProjectName = proj.Project,
+                              EstimatedHours = tsk.EstimatedHours,
+                              ConsumedHours = tsk.ConsumedHours,
+                              Deviation = tsk.Deviation,
+                              Status = tsk.Status
+
+                          }).ToList();
             return Request.CreateResponse(HttpStatusCode.OK, result);
         }
+    
+    public HttpResponseMessage Put(int id)
+    {
+        ProjectManagerEntities1 db = new ProjectManagerEntities1();
+            var result = db.Task_tbl.SingleOrDefault(b => b.TaskId == id);
+            if (result != null)
+            {
+                result.Status = "Changed";
+                db.SaveChanges();
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
     }
 }
+
+
